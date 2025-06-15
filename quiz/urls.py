c58e8e1ed802c396c58a61c651aa86537835a952
Flavie Lécrivain <flavie30.12@gmail.com>
2025-06-15 19:05:23 +0200

    Mise à jour du quiz Django

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quiz_view, name='quiz'),  # <-- pour l'URL /quiz/
-    path('resultat/', views.result_view, name='resultat'),  # si tu envoies ici les résultats
+    path('quiz/', views.quiz_view, name='quiz')  # <-- pour l'URL /quiz/
 ]
